2cb3eccdebdec749debaeda85eb8ef480cf2e2a5
kajtek <kajtek0123@gmail.com>
2024-11-30 18:55:39 +0100

    refactor: overall js code refactor

--- GameDexApp/assets/js/app.js
+++ GameDexApp/assets/js/app.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const API_URL = 'https://pokeapi.co/api/v2/pokemon/';
-const pokemonList = document.getElementById('list');
-const pokemonDetails = document.getElementById('info');
-const searchBar = document.getElementById('search');
+
+const pokemon_list = document.getElementById('list');
+const pokemon_details = document.getElementById('details');
+const search_bar = document.getElementById('search');
 const loading = document.getElementById('loading');
 
+const pokemon_list_size = 20
+
+// run the app
+async function run() {
+    fetchPokemonList()
+        .then(data => displayPokemonList(data))
+        .catch(error => console.error(error));
+    
+}
+
+// show or hide loading "spinner" 
 function displayLoading(isLoading) {
     loading.style.display = isLoading ? 'block' : 'none';
 }
@@ -14,14 +26,17 @@ function displayLoading(isLoading) {
 async function fetchPokemonList() {
     try {
         displayLoading(true);
-        const response = await fetch(`${API_URL}?limit=20`);
 
-        if (!response.ok) throw new Error('Nie udało sie pobrac listy pokemonow');
+        // fetch pokemon list of size pokemon_list_size
+        const response = await fetch(`${API_URL}?limit=${pokemon_list_size}`);
+
+        if (!response.ok) throw new Error('Could not retrieve pokemon list');
 
         const data = await response.json();
-        displayPokemonList(data.results);
+        
+        return data.results;
     } catch (error) {
-        console.error(error);
+        throw error;
     } finally {
         displayLoading(false);
     }
@@ -29,40 +44,49 @@ async function fetchPokemonList() {
 
 // display pokemon list
 function displayPokemonList(data) {
-    pokemonList.innerHTML = '';
+    pokemon_list.innerHTML = '';
 
+    // create a list item for each pokemon
     data.forEach((pokemon, index) => {
-        const listItem = document.createElement('li');
+        const li = document.createElement('li');
 
-        listItem.innerHTML = `
-            <h3>${pokemon.name} (#${index + 1})</h3>
+        li.innerHTML = `
+            <h3>${pokemon.name} #${index + 1}</h3>
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png" alt="${pokemon.name}">
         `;
 
-        listItem.addEventListener('click', () => fetchPokemonDetails(index + 1));
+        // fetch pokemon details on click and display them
+        li.addEventListener('click', () => fetchPokemonDetails(index + 1)
+                .then(data => displayPokemonDetails(data))
+                .catch(error => console.error(error)));
 
-        pokemonList.appendChild(listItem);
+        pokemon_list.appendChild(li);
     });
 }
 
-// get pokemon info
+// get pokemon details
 async function fetchPokemonDetails(id) {
     try {
         displayLoading(true);
+
+        // fetch chosen pokemon details
         const response = await fetch(`${API_URL}${id}`);
-        if (!response.ok) throw new Error("Nie udało się pobrać informacji o pokemonie");
+
+        if (!response.ok) throw new Error(`Couldn't retrieve #${id} pokemon details`);
+
         const data = await response.json();
-        displayPokemonDetails(data);
+
+        return data;
     } catch (error) {
-        console.error(error);
+        throw error;
     } finally {
         displayLoading(false);
     }
 }
-// display pokemon info
+// display pokemon details
 function displayPokemonDetails(pokemon) {
-    pokemonDetails.innerHTML = `
-        <h3>${pokemon.name} (#${pokemon.id})</h3>
+    pokemon_details.innerHTML = `
+        <h3>${pokemon.name}</h3>
         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
         <p><strong>Type:</strong> ${pokemon.types.map(type => type.type.name).join(', ')}</p>
         <p><strong>Height:</strong> ${pokemon.height / 10} m</p>
@@ -74,13 +98,16 @@ function displayPokemonDetails(pokemon) {
     `;
 }
 
-searchBar.addEventListener('input', (event) => {
-    const searchValue = searchBar.ariaValueMax.toLowerCase();
-    const pokemonItems = document.querySelectorAll('#list li');
-    pokemonItems.forEach(item => {
-        const pokemonName = item.querySelector('span').textContent.toLowerCase();
-        item.style.display = pokemonName.includes(searchValue) ? 'flex' : 'none';
+search_bar.addEventListener('input', (event) => {
+    // get search value
+    const search_val = search_bar.value.toLowerCase();
+    // get all pokemon items from the list
+    const pokemon_items = document.querySelectorAll('#list li');
+    // filter pokemon items based on search value
+    pokemon_items.forEach(item => {
+        const pokemon_name = item.querySelector('h3').textContent.toLowerCase();
+        item.style.display = pokemon_name.includes(search_val) ? 'flex' : 'none';
     })
 })
 
-fetchPokemonList();
+run();
